0f0f419f869db605e450f85345b7b4fd9ab589d2
Beanie <phamhien7295@gmail.com>
2023-07-23 20:14:34 +0700

    cm

--- cypress/api_test/PR_01_POST.js
+++ cypress/api_test/PR_01_POST.js
@@ -1,12 +1,12 @@
 import { METHOD } from "../utils/Methods";
-import { items } from "../fixtures/db";
+import items from "../fixtures/db.json";
 
 describe('Post request', () => {
     it('Validate status code', () => {
         cy.request({
-            url: "http://localhost:3000/posts",
+            url: "http://localhost:3000/comments",
             method: METHOD.post,
-            body: items.posts
+            body: items
         }).then (res => {
             let {status, body} = res
             let responseBody = JSON.parse(JSON.stringify(body))
--- cypress/api_test/TC_04_DELETE_request.js
+++ cypress/api_test/TC_04_DELETE_request.js
@@ -1,10 +1,11 @@
 import {METHOD} from '../utils/Methods'
-
+import items from "../fixtures/db.json";
 describe('Test DELETE method', () => {
     it('send Delete and verify', () => {
         cy.request({
             method: METHOD.delete,
-            url: 'https://jsonplaceholder.typicode.com/posts/1'
+            url: 'http://localhost:3000/comments/',
+            body: items.postId
         }).then(res => {
             let {status} = res
             expect(status).to.eq(200, 'Verify status')
